0a6f9c8b336afd3a8034179c12245235549ead37
Paulo RFZ <paulorfz1122@gmail.com>
2024-07-17 22:31:11 -0300

    pessoa Maria

--- x/testando.c
+++ x/testando.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main () {
+int main ()
+{
+    char nome[50];
+    char sexo;
+    double salario;
+    int idade;
 
-    double x;
-    x = 2.3456;
-    printf("%.2lf\n", x);
+    idade = 32;
+    salario = 4560.9;
+    strcpy(nome, "Maria Silva");
+    sexo = 'F';
+
+    printf("Ola, meu nome e %s, sexo %c, tenho %i anos e ganho %.2lf.", nome, sexo, idade, salario);
+
+    return 0;
 }
