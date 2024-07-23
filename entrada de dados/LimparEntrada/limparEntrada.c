48394522ca6f12959106c2f9c56f57f2039d123e
Paulo RFZ <paulorfz1122@gmail.com>
2024-07-23 17:35:40 -0300

    limpar entrada e ler texto

--- entrada de dados/LimparEntrada/limparEntrada.c
+++ entrada de dados/LimparEntrada/limparEntrada.c
@@ -6,33 +6,44 @@ void limpar_entrada()
     char c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
+
 void ler_texto(char *buffer, int length)
 {
     fgets(buffer, length, stdin);
     strtok(buffer, "\n");
 }
 
-int main ()
+int main()
 {
-    int idade1, idade2;
+    double salario1, salario2;
     char nome1[50], nome2[50];
+    int idade;
+    char sexo;
 
-    printf("Escreva a idade1: ");
-    scanf("%i", &idade1);
-    limpar_entrada();
-    printf("Escreva o nome1: ");
+    printf("Nome da primeira pessoa: ");
     ler_texto(nome1, 50);
+    printf("Salario da primeira pessoa: ");
+    scanf("%lf", &salario1);
 
-    printf("Escreva a idade2: ");
-    scanf("%i", &idade2);
+    printf("Nome da segunda pessoa: ");
     limpar_entrada();
-    printf("Escreva o nome2: ");
     ler_texto(nome2, 50);
+    printf("Salario da segunda pessoa: ");
+    scanf("%lf", &salario2);
 
-    printf("Idade1: %i\n", idade1);
-    printf("Nome1: %s\n", nome1);
-    printf("Idade2: %i\n", idade2);
-    printf("Nome2: %s\n", nome2);
+    printf("Digite uma idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite um sexo (F/M): ");
+    limpar_entrada();
+    scanf("%c", &sexo);
+
+    printf("Nome 1: %s\n", nome1);
+    printf("Salario 1: %.2lf\n", salario1);
+    printf("Nome 2: %s\n", nome2);
+    printf("Salario 2: %.2lf\n", salario2);
+    printf("Idade: %d\n", idade);
+    printf("Sexo: %c\n", sexo);
 
     return 0;
 }
