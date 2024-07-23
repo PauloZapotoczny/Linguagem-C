33d95268a32fcf2975d2c0f693503aecf7a79897
Paulo RFZ <paulorfz1122@gmail.com>
2024-07-23 17:24:47 -0300

    funcao ler nome

--- entrada de dados/LimparEntrada/limparEntrada.c
+++ entrada de dados/LimparEntrada/limparEntrada.c
@@ -6,20 +6,33 @@ void limpar_entrada()
     char c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
+void ler_texto(char *buffer, int length)
+{
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
 
 int main ()
 {
-    int idade;
-    char nome[50];
+    int idade1, idade2;
+    char nome1[50], nome2[50];
 
-    printf("Escreva a idade: ");
-    scanf("%i", &idade);
+    printf("Escreva a idade1: ");
+    scanf("%i", &idade1);
     limpar_entrada();
-    printf("Escreva o nome: ");
-    fgets(nome, 50, stdin);
+    printf("Escreva o nome1: ");
+    ler_texto(nome1, 50);
 
-    printf("Idade: %i\n", idade);
-    printf("Nome: %s\n", nome);
+    printf("Escreva a idade2: ");
+    scanf("%i", &idade2);
+    limpar_entrada();
+    printf("Escreva o nome2: ");
+    ler_texto(nome2, 50);
+
+    printf("Idade1: %i\n", idade1);
+    printf("Nome1: %s\n", nome1);
+    printf("Idade2: %i\n", idade2);
+    printf("Nome2: %s\n", nome2);
 
     return 0;
 }
